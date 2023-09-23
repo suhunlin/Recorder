60d3262919ba6a73da340498ac8e54e71327bcf0
suhunlin <lin.suhun@msa.hinet.net>
2023-09-23 23:19:54 +0800

    1.Add recorder file path in file_provider_paths.xml 2.Add provider setting in AndroidManifest.xml 3.Complete File object add and path setting in RecorderService.java

--- app/src/main/java/com/suhun/recorder/RecorderService.java
+++ app/src/main/java/com/suhun/recorder/RecorderService.java
@@ -2,10 +2,23 @@ package com.suhun.recorder;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.os.IBinder;
 
+import java.io.File;
+import java.sql.Time;
+
 public class RecorderService extends Service {
+    private String tag = RecorderService.class.getSimpleName();
+    private MediaRecorder mediaRecorder;
+    private File saveDir, rfile;
+
     public RecorderService() {
+        long time = System.currentTimeMillis();
+        String childName = time+".3gp";
+        saveDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+        rfile = new File(saveDir, childName);
     }
 
     @Override
@@ -17,8 +30,12 @@ public class RecorderService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setOutputFile(rfile);
     }
-
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         return super.onStartCommand(intent, flags, startId);
