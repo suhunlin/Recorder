e91cabe4987fc0bdd2c6c1c962cb85a68baadc2c
suhunlin <lin.suhun@msa.hinet.net>
2023-09-23 19:49:22 +0800

    Add layout

--- app/src/main/java/com/suhun/recorder/MainActivity.java
+++ app/src/main/java/com/suhun/recorder/MainActivity.java
@@ -3,12 +3,22 @@ package com.suhun.recorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startRecorderFun(View view){
+
+    }
+
+    public void stopRecorderFun(View view){
+
     }
 }
