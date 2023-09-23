79f5e7b241181066387f625f69064bcf2d7a1837
suhunlin <lin.suhun@msa.hinet.net>
2023-09-23 20:27:13 +0800

    1.Add checkUserPermissionAboutRecorder() to check if user agree READ_EXTERNAL_STORAGE and WRITE_EXTERNAL_STORAGE and RECORD_AUDIO.
    issue2.If user not agree, send requestPermissions() to user
    
    3.Add callback method onRequestPermissionsResult() to
    response issue 2

--- app/src/main/java/com/suhun/recorder/MainActivity.java
+++ app/src/main/java/com/suhun/recorder/MainActivity.java
@@ -1,7 +1,11 @@
 package com.suhun.recorder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,6 +16,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(checkUserPermissionAboutRecorder()){
+            initRecorder();
+        }else{//三個permission如果有一個user沒有同意就發出requestPermissions請求使用者同意
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, 123);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 123){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED
+                    && grantResults[1]==PackageManager.PERMISSION_GRANTED
+                        &&grantResults[2]==PackageManager.PERMISSION_GRANTED)   {
+                initRecorder();
+            }else{
+                finish();
+            }
+        }
+    }
+
+    private boolean checkUserPermissionAboutRecorder(){
+        boolean result = true;
+        //三個permission如果有一個user沒有同意就發出請求
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ||
+            ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ||
+            ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED){
+            result = false;
+        }
+        return result;
+    }
+
+
+
+    private void initRecorder(){
+
     }
 
     public void startRecorderFun(View view){
